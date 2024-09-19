50ceccb1c0290369520ab3b68f1a222ce1977e31
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-09-19 18:12:07 +0200

    Specify code format style

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,11 @@ import 'package:persistent_user_dir_access_android/persistent_user_dir_access_an
 
 void main() => runApp(const MaterialApp(home: App()));
 
+/// Sample app showcasing persistent_user_dir_access_android plugin.
 class App extends StatefulWidget {
+  /// Create sample app showcasing persistent_user_dir_access_android plugin.
+  ///
+  /// Requires [MaterialApp] ancestor.
   const App({
     super.key,
     this.userDirs = const PersistentUserDirAccessAndroid(),
@@ -23,34 +27,27 @@ class _AppState extends State<App> {
   String? _uri;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('User dir sample app'),
-      ),
-      body: ListView(
-        children: [
-          Card(child: Text('Selected directory: $_uri')),
-          ListTile(
-            title: const Text('requestDirectoryUri()'),
-            onTap: () async {
-              final dirUri = await widget.userDirs.requestDirectoryUri();
-              setState(() {
-                _uri = dirUri;
-              });
-            },
-          ),
-          ListTile(
-            title: const Text("writeFile(uri, 'test.txt', 'text/plain', utf8.encode('Test text'))"),
-            onTap: _uri == null ? null : () async {
-              await widget.userDirs.writeFile(_uri!, 'test.txt', 'text/plain', utf8.encode('Test text'));
-            },
-          )
-        ],
-      )
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text('User dir sample app'),
+    ),
+    body: ListView(
+      children: [
+        Card(child: Text('Selected directory: $_uri')),
+        ListTile(
+          title: const Text('requestDirectoryUri()'),
+          onTap: () async {
+            final dirUri = await widget.userDirs.requestDirectoryUri();
+            setState(() {_uri = dirUri;});
+          },
+        ),
+        ListTile(
+          title: const Text("writeFile(uri, 'test.txt', 'text/plain', utf8.encode('Test text'))"),
+          onTap: _uri == null ? null : () async {
+            await widget.userDirs.writeFile(_uri!, 'test.txt', 'text/plain', utf8.encode('Test text'));
+          },
+        )
+      ],
+    )
+  );
 }
-
-
-
